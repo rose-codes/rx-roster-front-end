6a2971b580862e510d6c750e5a4449e40f1a5a57
Rose Le <rosehle@protonmail.com>
2023-02-02 00:23:22 -0800

    Finished adding validation to form, added basic styling for errors

--- rx-roster-front-end/src/components/NewMedForm.js
+++ rx-roster-front-end/src/components/NewMedForm.js
@@ -1,16 +1,19 @@
-import { useState, useContext } from "react";
-import { useForm, useController } from "react-hook-form";
-import * as yup from "yup";
+import { useContext } from "react";
+import { useForm } from "react-hook-form";
 import RxContext from "../context/RxContext";
+
+let renderCount = 0;
+
 const NewMedForm = () => {
   const {
     register,
     handleSubmit,
-    watch,
-    formState: { errors },
+    formState: { errors, isValid },
+    reset,
   } = useForm({
     defaultValues: {
-      medName: "",
+      brandName: "",
+      genericName: "",
       strength: 0,
       strengthUnits: "",
       quantity: 0,
@@ -24,234 +27,260 @@ const NewMedForm = () => {
       startDate: "",
       appearance: "",
       currentlyTaking: "",
-      startDate: "",
       sideEffects: "",
     },
+    mode: "onBlur",
   });
   const { addMed } = useContext(RxContext);
-  const [medName, setMedName] = useState("");
-  const [btnDisabled, setBtnDisabled] = useState(true);
-  const [message, setMessage] = useState("");
-  const handleTextChange = (e) => {
-    if (medName === "") {
-      setBtnDisabled(true);
-      setMessage(null);
-    } else if (medName !== "" && medName.trim().length <= 3) {
-      setMessage("Medication name must contain at least 3 characters");
-      setBtnDisabled(true);
-    } else {
-      setMessage(null);
-      setBtnDisabled(false);
-    }
-    setMedName(e.target.value);
-  };
 
   const onSubmit = (formValues) => {
-    // if (medName.trim().length > 10) {
-    //   const newMed = {
-    //     medName,
-    //   };
-    //   addMed(newMed);
-    // }
-    console.log(formValues);
+    addMed(formValues);
+    reset();
   };
+  renderCount++;
+  // @todo: separate each field + label into separate divs
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
+      <div>{renderCount}</div>
+      <label htmlFor="genericName">Generic Name*:</label>
       <input
+        className={`text-field ${errors.genericName && "invalid"}`}
         type="text"
-        {...register("medName")}
-        // {...register("medName", {
-        //   required: { value: true, message: "Medication Name is Required" },
-        // },
-        // {minLength: {
-        //   value: 3,
-        //   message: "Medication names must be longer than 3 characters"
-        // }})}
-        onChange={handleTextChange}
-        placeholder="Medication Name"
+        {...register(
+          "genericName",
+          {
+            required: { value: true, message: "Generic Name is Required" },
+            minLength: { value: 3, message: "test" },
+          },
+          {
+            minLength: {
+              value: 3,
+              message: "Medication names must be longer than 3 characters",
+            },
+          }
+        )}
+        placeholder="Generic Name"
       ></input>
+      <small className="error-message">
+        {errors?.genericName && errors.genericName.message}
+      </small>
+      <label htmlFor="brandName">Brand Name*:</label>
       <input
+        className={`text-field ${errors.brandName && "invalid"}`}
+        type="text"
+        {...register(
+          "brandName",
+          {
+            required: { value: true, message: "Brand Name is Required" },
+            minLength: { value: 3, message: "test" },
+          },
+          {
+            minLength: {
+              value: 3,
+              message: "Medication names must be longer than 3 characters",
+            },
+          }
+        )}
+        placeholder="Brand Name"
+      ></input>
+      <small className="error-message">
+        {errors?.brandName && errors.brandName.message}
+      </small>
+      <label htmlFor="strength">Strength*:</label>
+      <input
+        className={`text-field ${errors.strength && "invalid"}`}
         type="number"
-        {...register("strength")}
-        // {...register(
-        //   "strength",
-        //   { required: { value: true, message: "Please enter a number." } },
-        //   {
-        //     valueAsNumber: {
-        //       value: true,
-        //       message: "Please enter a valid number.",
-        //     },
-        //   },
-        //   {
-        //     min: {
-        //       value: 0,
-        //       message: "Medication strengths must be greater than 0."
-        //     }
-        //   }
-        // )}
-        onChange={handleTextChange}
+        step="0.001"
+        {...register("strength", {
+          required: { value: true, message: "Please enter a number." },
+          valueAsNumber: {
+            value: true,
+            message: "Please enter a valid number.",
+          },
+          min: {
+            value: 0,
+            message: "Medication strengths must be greater than 0.",
+          },
+        })}
         placeholder="Strength"
       ></input>
-      <label htmlFor="strength-units">Units:</label>
+      <small className="error-message">
+        {errors?.strength && errors.strength.message}
+      </small>
+      <label htmlFor="strength-units">Strength Units*:</label>
       <select
         id="strength-units"
-        {...register("strengthUnits")}
-        // {...register("strengthUnits", {
-        //   required: {
-        //     value: true,
-        //     message: "Please select units",
-        //   },
-        // })}
+        className={`text-field ${errors.strengthUnits && "invalid"}`}
+        {...register("strengthUnits", {
+          required: {
+            value: true,
+            message: "Please select valid units",
+          },
+        })}
       >
         <option value="">---</option>
         <option value="mg">mg</option>
         <option value="mL">mL</option>
+        <option value="%">%</option>
       </select>
+      <small className="error-message">
+        {errors?.strengthUnits && errors.strengthUnits.message}
+      </small>
+      <label htmlFor="quantity">Quantity*:</label>
       <input
-        onChange={handleTextChange}
+        className={`text-field ${errors.quantity && "invalid"}`}
         type="number"
-        {...register("quantity")}
-        // {...register(
-        //   "quantity",
-        //   { required: { value: true, message: "Please enter a number" } },
-        //   {
-        //     valueAsNumber: {
-        //       value: true,
-        //       message: "Please enter a valid number",
-        //     },
-        //   },
-        //   {
-        //     min: {
-        //       value: 0,
-        //       message: "Medication strengths must be greater than 0."
-        //     }
-        //   }
-        // )}
+        {...register("quantity", {
+          required: { value: true, message: "Please enter a number" },
+          valueAsNumber: {
+            value: true,
+            message: "Please enter a valid number",
+          },
+          min: {
+            value: 0,
+            message: "Medication strengths must be greater than 0.",
+          },
+        })}
         placeholder="Quantity"
       ></input>
-      <label htmlFor="quanity-units">Units:</label>
+      <small className="error-message">
+        {errors?.quantity && errors.quantity.message}
+      </small>
+      <label htmlFor="quanity-units"> Quantity Units*:</label>
       <select
+        className={`text-field ${errors.quantityUnits && "invalid"}`}
         id="quantity-units"
-        {...register("quantityUnits")}
-        // {...register("quantityUnits", {
-        //   required: {
-        //     value: true,
-        //     message: "Please select a units option",
-        //   },
-        // })}
+        {...register("quantityUnits", {
+          required: {
+            value: true,
+            message: "Please select a units option",
+          },
+        })}
       >
         <option value="base">---</option>
         <option value="tablet">Tablet</option>
         <option value="capsule">Capsule</option>
         <option value="caplet">Caplet</option>
+        <option value="mg">mg</option>
+        <option value="g">g</option>
         <option value="mL">mL</option>
         <option value="dropper">Dropper</option>
         <option value="suppository">Suppository</option>
         <option value="box">Box</option>
         <option value="packet">Packet</option>
+        <option value="tube">Tube</option>
       </select>
-      <label htmlFor="med-forms">Medication Form:</label>
+      <small className="error-message">
+        {errors?.quantityUnits && errors.quantityUnits.message}
+      </small>
+      <label htmlFor="med-forms">Medication Form*:</label>
+      {/* @todo: alphabetize options */}
       <select
+        className={`text-field ${errors.medForms && "invalid"}`}
         id="med-forms"
-        {...register("medForms")}
-        // {...register("medForms", {
-        //   required: {
-        //     value: true,
-        //     message: "Please select the form of the medication",
-        //   },
-        // })}
+        {...register("medForms", {
+          required: {
+            value: true,
+            message: "Please select the form of the medication",
+          },
+        })}
       >
+        <option value="">---</option>
         <option value="tablet">Tablet</option>
         <option value="capsule">Capsule</option>
         <option value="caplet">Caplet</option>
         <option value="liquid">Liquid</option>
         <option value="dropper">Dropper</option>
         <option value="suppository">Suppository</option>
+        <option value="cream">Cream</option>
+        <option value="ointment">Ointment</option>
       </select>
+      <small className="error-message">
+        {errors?.medForms && errors.medForms.message}
+      </small>
+      <label htmlFor="prescribedFor">Prescribed For:</label>
       <input
-        onChange={handleTextChange}
         type="text"
         {...register("prescribedFor")}
         placeholder="What was the medication prescribed/taken for?"
       ></input>
+      <label htmlFor="instructions">Instructions:</label>
       <input
-        onChange={handleTextChange}
         type="text"
         {...register("instructions")}
         placeholder="Instructions"
       ></input>
+      <label htmlFor="prescriber">Prescriber:</label>
       <input
-        onChange={handleTextChange}
         type="text"
         {...register("prescriber")}
         placeholder="Prescriber"
       ></input>
       {/* @todo - create dropdown of user's commonly used pharmacies + allow them to add another */}
+      <label htmlFor="pharmacyField">Pharmacy:</label>
       <input
-        onChange={handleTextChange}
         type="text"
-        {...register("pharmacy")}
+        {...register("pharmacyFilled")}
         placeholder="Pharmacy"
       ></input>
+      <label htmlFor="manufacturer">Manufacturer:</label>
       <input
-        onChange={handleTextChange}
         type="text"
         {...register("manufacturer")}
         placeholder="Manufacturer"
       ></input>
       {/* @todo - if user provides manufacturer and other required fields, may be able to pre-populate with info from API */}
+      <label htmlFor="appearance">Appearance:</label>
       <input
-        onChange={handleTextChange}
         type="text"
         {...register("appearance")}
         placeholder="Appearance"
       ></input>
-      <label htmlFor="currently-taking">Taking Currently?:</label>
+      <label htmlFor="currently-taking">Taking Currently?*:</label>
       <select
         id="currently-taking"
-        {...register("currentlyTaking")}
-        // {...register("currentlyTaking", {
-        //   required: {
-        //     value: true,
-        //     message:
-        //       "Please indicate if you are currently taking the medication",
-        //   },
-        // })}
+        {...register("currentlyTaking", {
+          required: {
+            value: true,
+            message:
+              "Please indicate if you are currently taking the medication",
+          },
+        })}
       >
         <option value="">---</option>
         <option value="true">Yes</option>
         <option value="false">No</option>
       </select>
-      {/* @todo - reformat to date and not just a string */}
+      <label htmlFor="startDate">Date Medication Was Started:</label>
       <input
-        onChange={handleTextChange}
+        className={`text-field ${errors.startDate && "invalid"}`}
         type="text"
-        {...register("startDate")}
-        // {...register(
-        //   "startDate",
-        //   {
-        //     required: {
-        //       value: true,
-        //       message: "Please indicated when the medication was started.",
-        //     },
-        //   },
-        //   {
-        //     valueAsDate: {
-        //       value: true,
-        //       message: "Please provide a valid date",
-        //     },
-        //   }
-        // )}
+        {...register(
+          "startDate",
+          {
+            required: {
+              value: true,
+              message: "Please indicated when the medication was started.",
+            },
+          },
+          {
+            valueAsDate: {
+              value: true,
+              message: "Please provide a valid date",
+            },
+          }
+        )}
         placeholder="Date Medication Started"
       ></input>
+      <label htmlFor="sideEffects">Side Effects:</label>
       <input
-        onChange={handleTextChange}
         type="text"
         placeholder="Side Effects"
         {...register("sideEffects")}
       ></input>
-      <button type="submit">Submit Medication</button>
-      <div>{message && <div className="message">{message}</div>}</div>
+      <button type="submit" disabled={!isValid}>
+        Submit Medication
+      </button>
     </form>
   );
 };
