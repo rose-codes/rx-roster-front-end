d70758372c19d427be09dcd5204b1849c820324b
Rose Le <rosehle@protonmail.com>
2023-02-09 16:29:02 -0800

    Modified AboutPage

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -5,7 +5,7 @@ const AboutPage = () => {
     <div className="about">
       <h1>About This Project</h1>
       <p>This is a React app</p>
-      <p>Version: 1.0.0</p>
+      {/* <p>Version: 1.0.0</p> */}
       <p>
         <Link to="/">Back to Home</Link>
       </p>
