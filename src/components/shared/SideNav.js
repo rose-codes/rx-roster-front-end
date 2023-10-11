d65a33869a2d277d5bb727a8ebaeea9c2b7c94d8
Rose Le <rosehle@protonmail.com>
2023-10-11 13:04:34 -0700

    Overhaul sidebar

--- src/components/shared/SideNav.js
+++ src/components/shared/SideNav.js
@@ -1,169 +1,93 @@
 import { Link, useLocation } from "react-router-dom";
 import { useLogout } from "../../hooks/useLogout";
 import { useAuthContext } from "../../hooks/useAuthContext";
-import Dashboard from "../../pages/v2/Dashboard";
 import { CiPillsBottle1 } from "react-icons/ci";
 import { FaPills } from "react-icons/fa6";
 import { RxDashboard } from "react-icons/rx";
-import AllMedsPage from "../../pages/AllMedsPage";
 import { ReactSVG } from "react-svg";
 import { MdOutlineDescription } from "react-icons/md";
+import { GrLogout } from "react-icons/gr";
+import { useState } from "react";
+import Dashboard from "../../pages/v2/Dashboard";
+import AllMedsPage from "../../pages/AllMedsPage";
 import { NewMedFormPage } from "../../pages/v2/NewMedFormPage";
 
 const SideNav = () => {
   const { logout } = useLogout();
   const { user } = useAuthContext();
   const location = useLocation();
+  const [open, setOpen] = useState(true);
+  const Links = [
+    { title: "Dashboard", svg: <RxDashboard />, page: "/" },
+    {
+      title: "All Medications",
+      svg: <CiPillsBottle1 />,
+      page: "/all-medications",
+    },
+    {
+      title: "Current Medications",
+      svg: <FaPills />,
+      page: "/current-medications",
+    },
+    {
+      title: "About",
+      svg: <MdOutlineDescription />,
+      page: "https://github.com/rose-codes/rx-roster-front-end",
+    },
+    { title: "Logout", svg: <GrLogout />, page: "/" },
+  ];
 
   const handleClick = () => {
     logout();
   };
 
   return (
-    <div className="drawer lg:drawer-open">
-      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content flex flex-col justify-center">
-        {user && location.pathname === "/all-medications" && <AllMedsPage />}
-        {user && location.pathname === "/" && <Dashboard />}
-        {user && location.pathname === "/new-medication" && <NewMedFormPage />}
-      </div>
+    <div className="flex" data-theme="pastel">
       <div
-        data-theme="pastel"
-        className="drawer-side z-40 scroll-smooth scroll-pt-20 bg-neutral"
+        className={`w-20 md:w-80 duration-300 h-screen bg-neutral relative sidebar-container flex flex-col gap-y-4`}
       >
-        <label
-          htmlFor="drawer"
-          className="drawer-overlay"
-          aria-label="Close menu"
-        ></label>
-        <aside className="w-80">
-          {/* logo */}
-          <div className="side-logo">
-            <Link
-              to="/"
-              aria-current="page"
-              aria-label="Homepage"
-              className="btn btn-ghost !flex flex-0 px-2"
-              data-svelte-h="svelte-pw6yxt"
-            >
-              <ReactSVG src="/assets/rxLogo.svg" className="w-12 h-12 -ml-2" />
-              <div className="font-title text-lg md:text-2xl">
-                <span className="text-color-neutral-content">RxRoster</span>
-              </div>
-            </Link>
-          </div>
-          {/* Hidden Nav */}
-          <ul className="menu menu-horizontal w-full justify-between px-4 py-2 lg:hidden">
-            <li>
-              <Link
-                to="/"
-                className="navbar-link-content"
-                data-sveltekit-preload-data="hover"
-              >
-                <span>
-                  <RxDashboard />
-                </span>
-                <span>Dashboard</span>
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/all-medications"
-                className="navbar-link-content"
-                data-sveltekit-preload-data="hover"
-              >
-                <span>
-                  <FaPills />
-                </span>
-                <span>Current Medications</span>
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/all-medications"
-                className="navbar-link-content"
-                data-sveltekit-preload-data="hover"
-              >
-                <span>
-                  <CiPillsBottle1 />
-                </span>
-                <span>All Medications</span>
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/"
-                className="navbar-link-content"
-                data-sveltekit-preload-data="hover"
-              >
-                <span>
-                  <MdOutlineDescription fill="neutral-content" />
-                </span>
-                <span>About</span>
-              </Link>
-            </li>
-          </ul>
-          <div className="h-4"></div>
-          {/* Large NavBar */}
-          <ul className="menu menu-sm lg:menu-md px-4 py-0">
-            {/* Sidebar content here */}
-            <li>
-              <Link
-                to="/"
-                className="navbar-link-content"
-                data-sveltekit-preload-data="hover"
-              >
-                <span>
-                  <RxDashboard />
-                </span>
-                <span>Dashboard</span>
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/all-medications"
-                className="navbar-link-content"
-                data-sveltekit-preload-data="hover"
-              >
-                <span>
-                  <CiPillsBottle1 />
-                </span>
-                <span>All Medications</span>
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/all-medications"
-                className="navbar-link-content"
-                data-sveltekit-preload-data="hover"
-              >
-                <span>
-                  <FaPills />
-                </span>
-                <span>Current Medications</span>
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/"
-                className="navbar-link-content"
-                data-sveltekit-preload-data="hover"
-              >
-                <span>
-                  <MdOutlineDescription fill="neutral-content" />
-                </span>
-                <span>About</span>
-              </Link>
-            </li>
-            <li>
-              <button onClick={handleClick} className="mx-2">
-                <div className="-mt-1">Logout</div>
-              </button>
-            </li>
-          </ul>
-          <ul className="menu menu-sm lg:menu-md px-4 py-0"></ul>
-          <div className="bg-base-100 pointer-events-none sticky bottom-0 flex h-40 [mask-image:linear-gradient(transparent,#70ACC7)]"></div>
-        </aside>
+        <div className="side-logo my-8 pl-8">
+          <Link
+            to="/"
+            aria-current="page"
+            aria-label="Homepage"
+            className="btn btn-ghost !flex flex-0 px-2 justify-self-start items-center"
+            data-svelte-h="svelte-pw6yxt"
+          >
+            <ReactSVG src="/assets/rxLogo.svg" className="w-12 h-12 -ml-6" />
+            <div className="font-title text-xl md:text-3xl">
+              <span className="hidden md:inline-flex text-color-neutral-content`}">
+                <span className="capitalize">Rx</span>
+                <span className="capitalize">Roster</span>
+              </span>
+            </div>
+          </Link>
+        </div>
+        <ul>
+          {Links.map((link, index) => {
+            return (
+              <li className="my-3">
+                <Link
+                  to={link.page}
+                  className="navbar-link-content flex gap-x-4 justify-self-start items-center font-title text-md md:text-lg"
+                  data-sveltekit-preload-data="hover"
+                >
+                  <span>{link.svg}</span>
+                  <span className="hidden md:inline-flex">{link.title}</span>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="">
+        <div className="drawer-content flex flex-col justify-center">
+          {user && location.pathname === "/all-medications" && <AllMedsPage />}
+          {user && location.pathname === "/" && <Dashboard />}
+          {user && location.pathname === "/new-medication" && (
+            <NewMedFormPage />
+          )}
+        </div>
       </div>
     </div>
   );
