4d017e6efed6aedb387ee158bd14a37f4e3a8139
Rose Le <rosehle@protonmail.com>
2023-10-17 18:38:56 -0700

    Update hooks to be used with next

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,3 +1,4 @@
+"use client";
 import { useAuthContext } from "./useAuthContext";
 import { useRxContext } from "./useRxContext";
 
--- src/hooks/useRxContext.js
+++ src/hooks/useRxContext.js
@@ -1,3 +1,4 @@
+"use client";
 import { RxContext } from "../context/RxContext";
 import { useContext } from "react";
 
