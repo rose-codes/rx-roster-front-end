3cec591bce238502a3575691148b41281dc8d4d2
Rose Le <rosehle@protonmail.com>
2023-12-12 11:45:19 -0800

    Added responsive styling to login and register

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,16 +6,21 @@ import Link from "next/link";
 import AuthFormStyles from "../styles/authForms.module.css";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+import Footer from "../components/shared/Footer";
+import PhoneInput from "react-phone-number-input/react-hook-form-input";
 
 const LoginPage = () => {
   const { login } = useLogin();
   const router = useRouter();
-  const { dispatch, user } = useAuthContext();
+  const { user } = useAuthContext();
   const {
     register,
     handleSubmit,
     formState: { errors, isValid },
+    setError,
+    clearErrors,
     reset,
+    control,
   } = useForm({
     defaultValues: {
       email: "",
@@ -31,82 +36,87 @@ const LoginPage = () => {
     if (user) {
       router.push("/dashboard");
     }
-  }, [dispatch, user]);
+  }, [user]);
 
   return (
-    <>
+    <div>
       <NavBar />
-      <div className={AuthFormStyles.page_container}>
-        <div className={AuthFormStyles.content_container}>
-          <div className={AuthFormStyles.form_content_container}>
-            <div className={AuthFormStyles.title_container}>
-              <h1 className={AuthFormStyles.form_heading}>
-                Log In to RxRoster
-              </h1>
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-2xl sm:text-3xl mb-4 sm:mb-8 font-display">
+          Log In to RxRoster
+        </h1>
+        <div className="w-[min(100%,35rem)] bg-lime-50 p-4 border border-green-100/50 rounded-sm mb-4 sm:mb-8 font-body">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className={AuthFormStyles.input_group}>
+              <label className="mb-1 sm:mb-2 text-base sm:text-lg">
+                <span>Email</span>
+                <span className="text-red-500">*</span>
+              </label>
+              <input
+                type="email"
+                {...register("email", {
+                  required: {
+                    value: true,
+                    message: "Please enter an email.",
+                  },
+                  pattern: {
+                    value: /\S+@\S+\.\S+/,
+                    message: "Please enter a valid email",
+                  },
+                })}
+                placeholder="jane.doe@example.com"
+                className={AuthFormStyles.input_field}
+              ></input>
+              <small className={AuthFormStyles.error_message}>
+                {errors?.email && errors.email.message}
+              </small>
             </div>
-            <div className={AuthFormStyles.form_container}>
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <div className={AuthFormStyles.input_group}>
-                  <label>Email</label>
-                  <input
-                    type="email"
-                    {...register("email", {
-                      required: {
-                        value: true,
-                        message: "Please enter an email.",
-                      },
-                      pattern: {
-                        value: /\S+@\S+\.\S+/,
-                        message: "Please enter a valid email",
-                      },
-                    })}
-                    placeholder="jane.doe@example.com"
-                    className={AuthFormStyles.input_field}
-                  ></input>
-                  <small className={AuthFormStyles.error_message}>
-                    {errors?.email && errors.email.message}
-                  </small>
-                </div>
-                <div className={AuthFormStyles.input_group}>
-                  <label>Password</label>
-                  <input
-                    type="password"
-                    {...register("password", {
-                      required: {
-                        value: true,
-                        message: "Please enter a password.",
-                      },
-                    })}
-                    placeholder="Password"
-                    className={AuthFormStyles.input_field}
-                  ></input>
-                  <small className={AuthFormStyles.error_message}>
-                    {errors?.password && errors.password.message}
-                  </small>
-                </div>
-                <button
-                  type="submit"
-                  className={AuthFormStyles.submit_button}
-                  disabled={!isValid}
-                >
-                  Login
-                </button>
-              </form>
+            <div className={AuthFormStyles.input_group}>
+              <label className="mb-1 sm:mb-2 text-base sm:text-lg">
+                <span></span>
+                Password
+                <span className="text-red-500">*</span>
+              </label>
+              <input
+                type="password"
+                {...register("password", {
+                  required: {
+                    value: true,
+                    message: "Please enter a password.",
+                  },
+                })}
+                placeholder="Password"
+                className={AuthFormStyles.input_field}
+              ></input>
+              <small className={AuthFormStyles.error_message}>
+                {errors?.password && errors.password.message}
+              </small>
             </div>
-            <div className={AuthFormStyles.extra_text}>
-              <div>Don't have an account? Click below to sign up!</div>
-            </div>
-            <div className={AuthFormStyles.auth_redirect}>
-              <div className={AuthFormStyles.auth_redirect_content}>
-                <Link href="/register" className={AuthFormStyles.auth_link}>
-                  Join RxRoster!
-                </Link>
-              </div>
-            </div>
+
+            <button
+              type="submit"
+              className={AuthFormStyles.submit_button}
+              disabled={!isValid}
+            >
+              Login
+            </button>
+          </form>
+        </div>
+
+        <div className="mb-2 sm:mb-3">
+          Don't have an account? Click below to sign up!
+        </div>
+
+        <div className={AuthFormStyles.auth_redirect}>
+          <div className={AuthFormStyles.auth_redirect_content}>
+            <Link href="/register" className={AuthFormStyles.auth_link}>
+              Join RxRoster!
+            </Link>
           </div>
         </div>
       </div>
-    </>
+      <Footer />
+    </div>
   );
 };
 
--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -3,13 +3,19 @@ import Link from "next/link";
 import { useSignUp } from "../../hooks/useSignup";
 import AuthFormStyles from "../../styles/authForms.module.css";
 import NavBar from "../../components/shared/NavBar";
+import { useState } from "react";
+import PhoneInput from "react-phone-number-input/react-hook-form-input";
+import Footer from "../../components/shared/Footer";
 
 const RegisterPage = () => {
+  const [passFieldFocus, setPassFieldFocus] = useState(false);
   const {
     register,
     handleSubmit,
     formState: { errors, isValid },
+    getFieldState,
     reset,
+    control,
   } = useForm({
     defaultValues: {
       firstName: "",
@@ -52,139 +58,163 @@ const RegisterPage = () => {
   return (
     <>
       <NavBar />
-      <div className={AuthFormStyles.page_container}>
-        <div className={AuthFormStyles.content_container}>
-          <div className={AuthFormStyles.form_content_container}>
-            <div className={AuthFormStyles.title_container}>
-              <h1 className="py-3 my-3 text-black">Sign Up for RxRoster</h1>
-            </div>
-            <div className={AuthFormStyles.form_container}>
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <div className={AuthFormStyles.input_group}>
-                  <label>First Name:</label>
-                  <input
-                    type="text"
-                    {...register("firstName", {
-                      required: {
-                        value: true,
-                        message: "Please enter your first name.",
-                      },
-                    })}
-                    placeholder="First Name"
-                    className={AuthFormStyles.input_field}
-                  ></input>
-                </div>
-                <div className={AuthFormStyles.input_group}>
-                  <label>Last Name:</label>
-                  <input
-                    type="text"
-                    {...register("surname", {
-                      required: {
-                        value: true,
-                        message: "Please enter your surname.",
-                      },
-                    })}
-                    placeholder="Surname"
-                    className={AuthFormStyles.input_field}
-                  ></input>
-                </div>
-                <div className={AuthFormStyles.input_group}>
-                  <label>Date of Birth:</label>
-                  <input
-                    type="text"
-                    {...register("dateOfBirth", {
-                      // required: {
-                      //   value: true,
-                      //   message: "Please enter your Date of Birth.",
-                      // },
-                      valueAsDate: {
-                        value: true,
-                        message: "Please enter a valid date.",
-                      },
-                    })}
-                    // @todo: verify date format
-                    placeholder="MM/DD/YYYY"
-                    className={AuthFormStyles.input_field}
-                  ></input>
-                </div>
-                <div className={AuthFormStyles.input_group}>
-                  <label>Phone Number:</label>
-                  <input
-                    type="text"
-                    {...register("phoneNumber")}
-                    placeholder="XXX-XXX-XXXX"
-                    className={AuthFormStyles.input_field}
-                  ></input>
-                </div>
-                <div className={AuthFormStyles.input_group}>
-                  <label>Email:</label>
-                  <input
-                    type="email"
-                    {...register("email", {
-                      required: {
-                        value: true,
-                        message: "Please enter an email.",
-                      },
-                      pattern: {
-                        value: /\S+@\S+\.\S+/,
-                        message: "Please enter a valid email",
-                      },
-                    })}
-                    placeholder="Email"
-                    className={AuthFormStyles.input_field}
-                  ></input>
-                </div>
-                {/* @todo: Add Street Address fields */}
-                <div className={AuthFormStyles.input_group}>
-                  <label>Password:</label>
-                  <small className="text-sm">
-                    <div>Password must include:</div>
-                    <ul>
-                      <li>at least 8 characters</li>
-                      <li>at least one UPPERCASE letter</li>
-                      <li>at least one LOWERCASE letter</li>
-                      <li>at least one NUMBER</li>
-                      <li>at least one special character</li>
-                    </ul>
-                  </small>
-                  <input
-                    type="password"
-                    {...register("password", {
-                      required: {
-                        value: true,
-                        message: "Please enter a password.",
-                      },
-                      pattern: {
-                        value:
-                          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-                        message: "Please enter a valid password.",
-                      },
-                    })}
-                    placeholder="Password"
-                    className={AuthFormStyles.input_field}
-                  ></input>
-                </div>
+      <div className="flex flex-col items-center justify-center mb-14 sm:mb-16 ">
+        <h1 className="text-2xl sm:text-3xl mb-4 sm:mb-8 font-display">
+          Sign Up for RxRoster
+        </h1>
 
-                <button
-                  type="submit"
-                  disabled={!isValid}
-                  className={AuthFormStyles.submit_button}
-                >
-                  SIGN UP
-                </button>
-              </form>
+        <div className="w-[min(100%,40rem)] bg-lime-50 p-4 border border-green-100/50 rounded-sm mb-4 sm:mb-8 font-body">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col gap-2 sm:gap-4 sm:mb-2">
+              <label className="text-base sm:text-lg">
+                <span>First Name:</span>
+                <span className="text-red-500">*</span>
+              </label>
+              <input
+                type="text"
+                {...register("firstName", {
+                  required: {
+                    value: true,
+                    message: "Please enter your first name.",
+                  },
+                })}
+                placeholder="First Name"
+                className="p-2"
+              ></input>
             </div>
 
-            <div className={AuthFormStyles.auth_redirect}>
-              <div className={AuthFormStyles.auth_redirect_content}>
-                <Link href="/" className={AuthFormStyles.auth_link}>
-                  Already have an account?
-                </Link>
-              </div>
+            <div className="flex flex-col gap-2 sm:gap-4 sm:mb-2">
+              <label>
+                <span>Last Name:</span>
+                <span className="text-red-500">*</span>
+              </label>
+              <input
+                className="p-2"
+                type="text"
+                {...register("surname", {
+                  required: {
+                    value: true,
+                    message: "Please enter your surname.",
+                  },
+                })}
+                placeholder="Surname"
+              ></input>
             </div>
+            <div className="flex flex-col gap-2 sm:gap-4 sm:mb-2">
+              <label>
+                <span>Date of Birth:</span>
+                <span className="text-red-500">*</span>
+              </label>
+              <input
+                className="p-2"
+                type="text"
+                {...register("dateOfBirth", {
+                  // required: {
+                  //   value: true,
+                  //   message: "Please enter your Date of Birth.",
+                  // },
+                  valueAsDate: {
+                    value: true,
+                    message: "Please enter a valid date.",
+                  },
+                })}
+                // @todo: verify date format
+                placeholder="MM/DD/YYYY"
+              ></input>
+            </div>
+            <div className="flex flex-col gap-2 sm:gap-4 sm:mb-2">
+              <label>
+                <span>Phone Number:</span>
+                <span className="text-red-500">*</span>
+              </label>
+              <PhoneInput
+                className="p-2"
+                country="US"
+                name="phoneInput"
+                control={control}
+                rules={{ required: true }}
+                placeholder="(XXX)XXX-XXXX"
+              />
+            </div>
+
+            <div className="flex flex-col gap-2 sm:gap-4 sm:mb-2">
+              <label>
+                <span>Email:</span>
+                <span className="text-red-500">*</span>
+              </label>
+              <input
+                className="p-2"
+                type="email"
+                {...register("email", {
+                  required: {
+                    value: true,
+                    message: "Please enter an email.",
+                  },
+                  pattern: {
+                    value: /\S+@\S+\.\S+/,
+                    message: "Please enter a valid email",
+                  },
+                })}
+                placeholder="Email"
+              ></input>
+            </div>
+            {/* @todo: Add Street Address fields */}
+            <div className="flex flex-col gap-2 sm:gap-4 sm:mb-2">
+              <label>
+                <span>Password:</span>
+                <span className="text-red-500">*</span>
+              </label>
+              <input
+                className="p-2 mb-2 sm:mb-4"
+                type="password"
+                {...register("password", {
+                  required: {
+                    value: true,
+                    message: "Please enter a password.",
+                  },
+                  pattern: {
+                    value:
+                      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                    message: "Please enter a valid password.",
+                  },
+                })}
+                placeholder="Password"
+                onFocus={() => setPassFieldFocus(true)}
+                onBlur={() => setPassFieldFocus(false)}
+              ></input>
+              {passFieldFocus && (
+                <small className="text-sm mb-2 sm:mb-4">
+                  <div>Password must include:</div>
+                  <ul>
+                    <li>at least 8 characters</li>
+                    <li>at least one UPPERCASE letter</li>
+                    <li>at least one LOWERCASE letter</li>
+                    <li>at least one NUMBER</li>
+                    <li>at least one special character</li>
+                  </ul>
+                </small>
+              )}
+            </div>
+
+            <button
+              type="submit"
+              disabled={!isValid}
+              className={AuthFormStyles.submit_button}
+            >
+              SIGN UP
+            </button>
+          </form>
+        </div>
+
+        <div className={AuthFormStyles.auth_redirect}>
+          <div className={AuthFormStyles.auth_redirect_content}>
+            <Link href="/login" className={AuthFormStyles.auth_link}>
+              Already have an account? Sign in here.
+            </Link>
           </div>
         </div>
       </div>
+      <Footer />
     </>
   );
 };
